d0340561bf01d55094a6cd6f2ab999c6cac7ff3c
Nicovf23 <114188244+Nicovf23@users.noreply.github.com>
2023-02-10 11:35:47 -0400

    Add files via upload

---
+++ Calculadora de edad/calcular la edad/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace loteria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime fechaActual = DateTime.Today;
+            int edad = fechaActual.Year - dtpfechadenacimiento.Value.Year;
+            if (fechaActual < dtpfechadenacimiento.Value.AddYears(edad)) edad--;
+
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime fechaActual = DateTime.Today;
+            int edad = fechaActual.Year - dtpfechadenacimiento.Value.Year;
+            if (fechaActual < dtpfechadenacimiento.Value.AddYears(edad)) edad--;
+            lbledad.Text = edad.ToString() + "años";
+        }
+    }
+}
