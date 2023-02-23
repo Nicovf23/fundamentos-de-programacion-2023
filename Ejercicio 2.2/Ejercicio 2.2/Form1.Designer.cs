79c4ed164e8ee87357952e189525de0e6adb45a2
Nicovf23 <114188244+Nicovf23@users.noreply.github.com>
2023-02-22 22:55:27 -0400

    Add files via upload

---
+++ Ejercicio 2.2/Ejercicio 2.2/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Ejercicio_2._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstFrutas = new System.Windows.Forms.ListBox();
+            this.txtFruta = new System.Windows.Forms.TextBox();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstFrutas
+            // 
+            this.lstFrutas.FormattingEnabled = true;
+            this.lstFrutas.ItemHeight = 20;
+            this.lstFrutas.Items.AddRange(new object[] {
+            "Manzana",
+            "Aguacate",
+            "Pera"});
+            this.lstFrutas.Location = new System.Drawing.Point(154, 84);
+            this.lstFrutas.Name = "lstFrutas";
+            this.lstFrutas.Size = new System.Drawing.Size(234, 204);
+            this.lstFrutas.TabIndex = 0;
+            // 
+            // txtFruta
+            // 
+            this.txtFruta.Location = new System.Drawing.Point(177, 33);
+            this.txtFruta.Name = "txtFruta";
+            this.txtFruta.Size = new System.Drawing.Size(100, 26);
+            this.txtFruta.TabIndex = 1;
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(297, 33);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(91, 26);
+            this.btnAdicionar.TabIndex = 2;
+            this.btnAdicionar.Text = "Adicionar ";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.txtFruta);
+            this.Controls.Add(this.lstFrutas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstFrutas;
+        private System.Windows.Forms.TextBox txtFruta;
+        private System.Windows.Forms.Button btnAdicionar;
+    }
+}
+
---
+++ Ejercicio 2.2/Ejercicio 2.2/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_2._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Adicionar elementos 
+            lstFrutas.Items.Add("Banana");
+            lstFrutas.Items.Add("Durazno");
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            lstFrutas.Items.Add(txtFruta.Text);
+
+            lstFruta.Text = "string.Emty;
+        }
+    }
+}
