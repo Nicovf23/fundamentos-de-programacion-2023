254ec257a379c065b2101455741783aba3cbf8b0
Nicovf23 <114188244+Nicovf23@users.noreply.github.com>
2023-04-13 11:27:53 -0400

    Add files via upload

---
+++ Modos diferido e inmediato - 2/Modos diferido e inmediato - 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modos_diferido_e_inmediato___2
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+
+            int[]numeros = {1.5.4.7.6.3.5.9.8.11}
+
+            var valores = from n in numeros
+                          where n % 2 == 0
+                          select n;
+        }
+
+
+
+
+    }
+}
